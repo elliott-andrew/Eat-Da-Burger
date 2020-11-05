3ec6bc7bb432e49598e2d1d05e3c5e0643b03c5d
Andrew Elliott <andrewjc.elliott@gmail.com>
2020-11-05 17:18:36 -0500

    Updated tables to save and load burger names and if they've been eaten

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
-INSERT INTO cats (name) VALUES ('Meeses');
-INSERT INTO cats (name) VALUES ('Bobbi');
-INSERT INTO cats (name, sleepy) VALUES ('Sylvester', true);
-INSERT INTO cats (name, sleepy) VALUES ('Marilyn Manson', true);
-INSERT INTO cats (name, sleepy) VALUES ('Joe Biden', true);
-INSERT INTO cats (name) VALUES ('Owens');
+INSERT INTO burgers (name) VALUES ('Cheese Burger');
+INSERT INTO burgers (name) VALUES ('Hamburger');
+INSERT INTO burgers (name, eaten) VALUES ('Turkey Burger', true);
+INSERT INTO burgers (name, eaten) VALUES ('Veggie Burger', true);
+INSERT INTO burgers (name, eaten) VALUES ('Beyond Burger', true);
+INSERT INTO burgers (name) VALUES ('Bacon Cheese Burger');
